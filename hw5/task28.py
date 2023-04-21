e354b6daedecff7f9793f9777dd60531dab529d6
shaukat <shadvoll@gmail.com>
2023-04-21 14:36:51 +0300

    minor changes in logics

--- hw5/task28.py
+++ hw5/task28.py
@@ -1,7 +1,7 @@
 def sum_rec(a,b):
     if a == 0:
         return b
-    return sum_rec(a-1,b) + 1
+    return sum_rec(a-1,b+1)
 
 number1 = int(input('Первое слагаемое: '))
 number2 = int(input('Второе слагаемое: '))
